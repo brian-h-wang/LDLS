3c021f12ecf25a3e5f226e9077105ece8af3b72b
Brian H. Wang <bhw45@cornell.edu>
2019-03-08 17:30:34 -0500

    Fix pretrained model path

--- mask_rcnn/mask_rcnn.py
+++ mask_rcnn/mask_rcnn.py
@@ -51,7 +51,7 @@ class MaskRCNNDetector(object):
         MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
         # Local path to trained weights file
-        COCO_MODEL_PATH = os.path.join(ROOT_DIR, "lidar_segmentation/mask_rcnn_coco.h5")
+        COCO_MODEL_PATH = os.path.join(ROOT_DIR, "LDLS/mask_rcnn_coco.h5")
         # Download COCO trained weights from Releases if needed
         if not os.path.exists(COCO_MODEL_PATH):
             utils.download_trained_weights(COCO_MODEL_PATH)
