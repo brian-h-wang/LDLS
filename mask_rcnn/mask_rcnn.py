17a0f56d6eb52f5d471222d87d651085f46912fb
Brian H. Wang <bhw45@cornell.edu>
2019-03-08 17:33:29 -0500

    Fix for file paths

--- mask_rcnn/mask_rcnn.py
+++ mask_rcnn/mask_rcnn.py
@@ -20,7 +20,7 @@ config.inter_op_parallelism_threads = 1
 keras.backend.set_session(tensorflow.Session(config=config))
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../")
+ROOT_DIR = os.path.abspath(".")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -51,13 +51,10 @@ class MaskRCNNDetector(object):
         MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
         # Local path to trained weights file
-        COCO_MODEL_PATH = os.path.join(ROOT_DIR, "LDLS/mask_rcnn_coco.h5")
+        COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
         # Download COCO trained weights from Releases if needed
         if not os.path.exists(COCO_MODEL_PATH):
             utils.download_trained_weights(COCO_MODEL_PATH)
-
-        # Directory of images to run detection on
-        IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
         config = InferenceConfig()
         # config.display()
